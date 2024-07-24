0fdd4086067d08ec018333ca98100959c95aab5e
Bartosz Gierej <b.gierej@investa.pl>
2024-07-24 07:46:27 +0200

    Add generate grid function

---
+++ script.js
@@ -0,0 +1,31 @@
+function generateBlocks(number) {
+	const blockBorderSize = 1;
+	const blockSize = 14;
+
+	const container = document.querySelector(".container");
+	const containerSize = number * (blockSize + blockBorderSize * 2);
+
+	container.setAttribute(
+		"style",
+		"max-width:" + containerSize + "px; max-height:" + containerSize + "px;"
+	);
+
+	for (i = 0; i < number * number; i++) {
+		let div = document.createElement("div");
+		div.classList.add("block");
+		div.setAttribute(
+			"style",
+			"width: " +
+				blockSize +
+				"px; height: " +
+				blockSize +
+				"px; border: " +
+				blockBorderSize +
+				"px solid black"
+		);
+
+		container.appendChild(div);
+	}
+}
+
+generateBlocks(80);
